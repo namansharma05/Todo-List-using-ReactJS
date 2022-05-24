936cedc5f51a318d754b30e154298caea2db9452
Naman <namanks0511@gmail.com>
2022-05-25 00:39:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Todo from './components/todo';
+function App() {
+  return (
+    <>
+      <Todo/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/todo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Todo = ()=>{
+	return (
+		<>
+			<h1>hello world!</h1>
+		</>
+	)
+}
+export default Todo;
